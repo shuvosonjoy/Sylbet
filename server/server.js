a74f7a8b645afac926228e36010618c57a82b110
shuvosonjoy <shuvosdg123@gmail.com>
2026-05-30 13:49:21 +0600

    frontend

--- server/server.js
+++ server/server.js
@@ -13,7 +13,9 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
